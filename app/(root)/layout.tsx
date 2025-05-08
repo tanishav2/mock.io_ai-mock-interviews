f6a3024a66c00447a14ea1abe32c18d1c296d579
tanishav2 <tanisha.varshney2004@gmail.com>
2025-05-08 23:33:11 +0530

    Updated layout and tech icon components, added InterviewCard

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,18 @@
 import {ReactNode} from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
 const RootLayout = ({children} : {children : ReactNode}) => {
     return (
-        <div>{children}</div>
+        <div className = "root-layout">
+            <nav>
+                <Link href = "/" className = "fleex items-center gap-2">
+                    <Image src = "/logo.svg" alt = "logo" height = {32} width = {38}/>
+                    <h2 className = "text-primary-100">Mock.io</h2>
+                </Link>
+            </nav>
+            {children}
+        </div>
     )
 }
 export default RootLayout
